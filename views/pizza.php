f01e5c86113be495b196bfd78cedd3e7c896bcf6
dav-and-co <claude.daviatte@lapiscine.pro>
2024-07-12 17:05:02 +0200

    pizza améliorée

--- views/pizza.php
+++ views/pizza.php
@@ -6,8 +6,6 @@
 <?php require_once('../config/config.php');?>
 <?php require_once('../controller/commande-Controller.php'); ?> 
 
-
-
 <main>
 
 <!-- Créez un nouveau fichier contenant une classe Pizza.
@@ -15,11 +13,10 @@ Ajoutez en propriétés privées : size, price, base, ingredient1, ingredient2, 
 Faites en sorte que quand une nouvelle Pizza est créée (nouvelle instance), toutes les propriétés soient obligatoirement remplies, soit par l'utilisateur, soit calculées.
 Faites une méthode pay() qui permet de passer la pizza en status "payé" uniquement si le statut actuel est "en cours"
 Faites une méthode ship() qui permet de passer la pizza en status "livré" uniquement si le status actuel est "payé"
-
 Commentez votre code -->
 
 <?php
-class pizza {
+class Pizza {
 	// initialisation des données privées
 	private $price;
 	private $size;
@@ -53,7 +50,7 @@ class pizza {
 			$this->price=14;
 		}
 	}
-	function pay($montant) {
+	public function pay($montant) {
 		if ($this->statut==="en cours de commande") {
 			if ($montant===$this->price) {
 				echo "<p> C'est bon, merci ! </p>";
@@ -65,7 +62,7 @@ class pizza {
 			echo "<p> Commande non enregistrée </p>";
 		}
 	}
-	function ship() {
+	public function ship() {
 		if ($this->statut==="payé") {
 			$this->statut="livré";
 			echo "<p> livraison en cours  </p>";
@@ -73,14 +70,47 @@ class pizza {
 			echo "<p> pizza non payée </p>";
 		}
 	}
+	public function getingredients() {
+		return " les ingrédients : ".$this->ingredient1. " - ". $this->ingredient2. " - ".$this->ingredient3;
+	}
 
+	public function getdonnees() {
+		return " la taille est ". $this->size. " et son prix : ".$this->price;
+	}
+	public function gettableau() {
+		return [$this->ingredient1,$this->ingredient2,$this->ingredient3,$this->size,$this->price];
+	}
 }
 
-// commande de la pizza de Claude
-$claude = new pizza ("m", "creme","fromage","olives","comcombre");
-	var_dump($claude);
+// initialisation de l'instance de class pizza
+$claude = new Pizza ("m", "creme","fromage","olives","comcombre");
+
+// Créez une pizza avec new Pizza. Puis, affichez avec du HTML, la base de la pizza, les ingredients, 
+// la taille et le prix. Pour ça, générez des getters pour les propriétés que vous voulez utiliser en dehors de la classe.
+
+echo $claude -> getingredients();
+echo $claude -> getdonnees();
+
+$synthese =  $claude -> gettableau();
+$i = 0;
+foreach ($claude as $composant) {
+	$i=$i+1;
+	if ($i===5) {
+		echo "<h2> le prix est : ".$composant."</h2>";
+	} elseif ($i===4) {
+			echo "<p> la taille est : ".$composant."</p>";
+		} elseif ($i===1) {
+				echo "<p> la composition est : </p>";
+				echo "<p>".$composant."</p>";
+			} else {
+				echo "<p>";$composant."</p>";
+			}
+}
+
+
+
 $claude	-> pay(10);
-	var_dump($claude);
+
 $claude	-> ship();
 	var_dump($claude);
 
